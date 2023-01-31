3e70c36bedcdeedd9dbf31be8018841071508686
Tilak Kandel <tilakkndl2001@gmail.com>
2023-01-31 10:31:17 +0545

    added code for comparisionan and boolean

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
  <?php
-//loops
-//for loop
-for($i=0;$i<5;$i++){
-    echo "Loop is tobe run for 5th times". '<br/>';
-}
 
-//for each loop
-$blogs = ["blog1", "blog2", "blog3"];
-foreach($blogs as $blog){
-    echo $blog.'<br/>';
-    // echo "for each loop";
-}
+//comparision booleans (true or false)
+echo true;//"1"
+echo false;//""
+//numbers
+echo 5<10;
+echo 5==10;
+echo 5 != 10;
 
-//while loop
-$i=0;
-while($i<count($blogs)){
-    echo $blogs[$i];
-    $i++;
-}
+//strings
+echo 'shan'<'yoshi'; //true as s comes faster than y
+echo 'shan'>'Shan'; //true
+
+//loose vs strict equal comparisions
+echo 5 == "5" // true;
+echo 5 === "5"//false
 ?> 
 <!DOCTYPE html>
 <html lang="en">
