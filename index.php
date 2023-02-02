2c5124fac5071c53288f89d458b22db9a5a8e03b
Tilak Kandel <tilakkndl2001@gmail.com>
2023-02-02 17:30:37 +0545

    connect to the php my sql and get data from table pizza

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
  <?php
  
+ //connect to database
+ $conn = mysqli_connect("localhost", 'tilak', 'tilak', 'ninja_pizza');
+
+ //check connection
+ if(!$conn){
+    echo "Connection error". mysqli_connect_error();
+ }
+ 
+ //write query for all pizzas
+ $sql = 'SELECT title, ingridents, id FROM  pizzas';
+
+ //make query and get result
+ $result = mysqli_query($conn, $sql);
+
+ //fetch the resulting rows as the array
+ $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+ //free results from memory
+mysqli_free_result($result);
+
+ //close connection
+ mysqli_close($conn);
+ print_r($pizzas);
 ?> 
 <!DOCTYPE html>
 <html lang="en">
